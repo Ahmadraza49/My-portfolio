445dd2f02ddc6742f0b70b6d8e4435afb4445256
Ahmadraza49 <safdarahmad249gmail.com>
2026-05-22 22:55:47 +0500

    Updated CV

--- src/App.jsx
+++ src/App.jsx
@@ -177,7 +177,7 @@ export default function PortfolioLandingPage() {
             </a>
 
             <a
-             href="/cv.pdf"
+             href="/Ahmadraza--cv.pdf"
               download
               className="border border-white/20 px-5 py-2 rounded-full hover:border-green-500 hover:text-green-400 transition duration-300"
             >
